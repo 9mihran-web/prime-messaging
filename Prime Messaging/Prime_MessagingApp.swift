fd369531a5a7945eff94d7018443b9bc5adde84a
Mihran Gevorgyan <88gm888@gmail.com>
2026-03-15 21:27:21 +0400

    Initial Commit

---
+++ Prime Messaging/Prime_MessagingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prime_MessagingApp.swift
+//  Prime Messaging
+//
+//  Created by Mihran Gevorgyan on 15.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Prime_MessagingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Prime MessagingTests/Prime_MessagingTests.swift
@@ -0,0 +1,17 @@
+//
+//  Prime_MessagingTests.swift
+//  Prime MessagingTests
+//
+//  Created by Mihran Gevorgyan on 15.03.2026.
+//
+
+import Testing
+@testable import Prime_Messaging
+
+struct Prime_MessagingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
